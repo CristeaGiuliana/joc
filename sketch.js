d6818371ba65f0b37f6a6ddb4b56bd0a27a8a66e
giulianaa23 <giulianacristea06@gmail.com>
2024-04-16 13:30:17 +0300

    joc

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    fill(255, 255, 255); // Culoarea alb
+    ellipse(200, 200, 50); // Desenează o bilă la coordonatele (200, 200) cu diametrul 50
+}
