7b1ec3469be9485ed866a1bd65afeb4041dc18d4
giulianaa23 <giulianacristea06@gmail.com>
2024-04-16 14:04:27 +0300

    bile

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
 }
 
 function draw() {
     background(220);
     fill(255, 255, 255); // Culoarea alb
-    ellipse(200, 200, 50); // Desenează o bilă la coordonatele (200, 200) cu diametrul 50
+    ellipse(200, 200, 50); // Desenează o bilă la coordonatele (200, 200) cu diametrul 50 
+    ellipse(260, 200, 50);
+    ellipse(320, 200, 50);
+    ellipse(380, 200, 50);
+    fill(0, 0, 0);//culoarea negru
+    ellipse(140, 100, 50);
+
 }
